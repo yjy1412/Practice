7f3a0941790844fc180c0d821cc074b9b5840f39
yjy1412 <yjy852@gmail.com>
2022-02-19 22:44:27 +0900

    Feat: 스크롤 헤더 구현

--- cloneCoding_project1/assets/js/main.js
+++ cloneCoding_project1/assets/js/main.js
@@ -27,3 +27,11 @@ const linkAction = () => {
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// change background header
+function scrollHeader () {
+  const header = document.getElementById('header');
+  if (this.scrolly >= 100) header.classList.add('scroll-header');
+  else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
